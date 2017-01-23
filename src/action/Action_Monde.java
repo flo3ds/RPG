7815466cc06366089ec95e52a9b6bfc5c6e35863
ehll <ehll@Prog-ubuntu>
2017-01-23 23:11:29 +0100

    Flo ajout hache

--- src/action/Action_Monde.java
+++ src/action/Action_Monde.java
@@ -3,21 +3,23 @@ package action;
 import action.action_monde.Action_Faune;
 import monde.GenMonde;
 import perso.Personnage;
+import tools.Hache;
 
 public class Action_Monde extends Action_Perso {
 
 	public GenMonde monde;
-	public Action_Faune faune= new Action_Faune();
+	public Action_Faune faune = new Action_Faune();
+
 	public String analyseSol() {
 		return this.monde.sol.analyseSol();
 	}
-	
+
 	public String analyseFaune(Personnage perso) {
-		perso.position= Position.faune;
-		
+		perso.position = Position.faune;
+
 		return this.monde.faune.getAllDescription();
 	}
-	
+
 	public String analyseFlore() {
 		return this.monde.flore.getAllDescription();
 	}
@@ -31,19 +33,20 @@ public class Action_Monde extends Action_Perso {
 		return "Vous minez " + this.monde.sol.minerais.getNombre() + "K de " + this.monde.sol.minerais.getMatiere()
 				+ ".\n";
 	}
-	
+
 	public String couperBois(Personnage perso) {
-		perso.inv.putItem(this.monde.flore.arbres[0].bois);
-		return "Vous coupez " + this.monde.flore.arbres[0].bois.getNombre() +" bois.\n";
+		if (perso.inv.haveItem(new Hache())) {
+			perso.inv.putItem(this.monde.flore.arbres[0].bois);
+			return "Vous coupez " + this.monde.flore.arbres[0].bois.getNombre() + " bois.\n";
+		} else
+			return "Vous n'avez pas de hache";
 	}
 
-	
-	
 	public static String base(Personnage perso) {
 		perso.position = Position.base;
 		return "Vous rentrez a votre base.\n";
 	}
-	
+
 	public static String arretChasse(Personnage perso) {
 		perso.position = Position.monde;
 		return "Vous revenez de votre chasse.\n";
@@ -66,29 +69,28 @@ public class Action_Monde extends Action_Perso {
 	}
 
 	public String action(Personnage perso, String in) {
-	    if(perso.position == Position.faune){
-	    	
-	  return this.faune.action(perso, in, this.monde);
-	 }else{
-	   if (Action.analyser_sol.test(in))
-	    return this.analyseSol();
-	   else if (Action.miner.test(in))
-	    return this.miner(perso);
-	   else if (Action.analyser_faune.test(in)){
-	    perso.position = Position.faune;
-	    System.out.println(Position.faune);
-	    return this.analyseFaune(perso);
-	   }
-	   else if (Action.analyser_flore.test(in))
-	    return this.analyseFlore();
-	   else if (Action.base.test(in))
-	    return this.base(perso);
-	   else if (this.actionPersoTest(in))
-	    return this.actionPerso(perso, in);
-	   else if (Action.couper_bois.test(in))
-	    return this.couperBois(perso);
-	   else
-	    return this.help();
-	   }
-	  }
-	 }
+		if (perso.position == Position.faune) {
+
+			return this.faune.action(perso, in, this.monde);
+		} else {
+			if (Action.analyser_sol.test(in))
+				return this.analyseSol();
+			else if (Action.miner.test(in))
+				return this.miner(perso);
+			else if (Action.analyser_faune.test(in)) {
+				perso.position = Position.faune;
+				System.out.println(Position.faune);
+				return this.analyseFaune(perso);
+			} else if (Action.analyser_flore.test(in))
+				return this.analyseFlore();
+			else if (Action.base.test(in))
+				return this.base(perso);
+			else if (this.actionPersoTest(in))
+				return this.actionPerso(perso, in);
+			else if (Action.couper_bois.test(in))
+				return this.couperBois(perso);
+			else
+				return this.help();
+		}
+	}
+}
---
+++ src/tools/Hache.java
@@ -0,0 +1,13 @@
+package tools;
+
+public class Hache extends Tool{
+
+	//Ici on met le nom de l'item.
+	//Il doit etre unique !
+	private String id = "hache";
+
+	public Hache(){
+		this.setId(this.id);
+	}
+	
+}
